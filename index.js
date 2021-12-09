34b419c6733a86797891b0a47930e21c95bf053a
splatterman74 <compeng74@gmail.com>
2021-12-09 14:41:13 -0500

    Implemented apple collision and snake length increase.

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
 
+class SnakePart{
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    }
+}
+
+const snakeParts = [];
+let tailLength = 2;
+
 let speed = 7;
 
 let tileCount = 20;
 let tileSize = canvas.width / tileCount - 2;
+
 let headX = 10;
 let headY = 10;
 
 let xVelocity = 0;
 let yVelocity = 0;
 
+let appleX = 5;
+let appleY = 5;
+
 //Game loop.
 function drawGame(){
     clearScreen();
     changeSnakePosition();
+    checkAppleCollision();
+    drawApple();
     drawSnake();
     setTimeout(drawGame, 1000/speed);
 }
@@ -29,7 +45,31 @@ function changeSnakePosition(){
     headY = headY + yVelocity;
 }
 
+function checkAppleCollision(){
+    if(appleX === headX && appleY === headY){
+        appleX = Math.floor(Math.random() * tileCount);
+        appleY = Math.floor(Math.random() * tileCount);
+        tailLength++;
+    }
+}
+
+function drawApple(){
+    ctx.fillStyle = 'red';
+    ctx.fillRect(appleX * tileCount, appleY * tileCount, tileSize, tileSize);
+}
+
 function drawSnake(){
+    ctx.fillStyle = 'green';
+    for(let i = 0; i < snakeParts.length; i++){
+        let part = snakeParts[i];
+        ctx.fillRect(part.x * tileCount, part.y * tileCount, tileSize, tileSize)
+    }
+
+    snakeParts.push(new SnakePart(headX, headY)); // Put item at end of list (ie. next to the head).
+    if(snakeParts.length > tailLength){
+        snakeParts.shift(); // Remove the part furthest from the head if we have more than the tailSize.
+    }
+
     ctx.fillStyle = 'orange';
     ctx.fillRect(headX * tileCount, headY * tileCount, tileSize, tileSize);
 }
